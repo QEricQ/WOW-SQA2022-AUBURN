8c08f5341bef6a139c9062272b09a5e7f3f4b8cc
QEricQ <114453425+QEricQ@users.noreply.github.com>
2022-12-01 20:54:29 -0600

    Update knn.py

--- TestOrchestrator4ML/label_perturbation_attack/knn.py
+++ TestOrchestrator4ML/label_perturbation_attack/knn.py
@@ -10,7 +10,6 @@ from sklearn.metrics import accuracy_score
 from sklearn.metrics import roc_curve, auc
 from matplotlib import pyplot as plt
 import logging_test
-import logging
 
 def euc_dist(x1, x2):
     return np.sqrt(np.sum((x1-x2)**2))
@@ -38,7 +37,7 @@ def predict(self, X_test):
         sorted_neigh_count = sorted(neigh_count.items(),    
         key=operator.itemgetter(1), reverse=True)
         predictions.append(sorted_neigh_count[0][0]) 
-    logO.debug('{}*{}*{}*{}'.format('knn.py', 'predict', str(a), str(b))) 
+    logO.debug('{}*{}*{}*{}'.format('knn.py', 'predict')) 
     return predictions
     
 def prepare_data():
