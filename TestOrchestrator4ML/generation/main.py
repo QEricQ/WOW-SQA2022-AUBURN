a662ed63da1476ceeb37951b05f4d04c8e9a6b29
QEricQ <114453425+QEricQ@users.noreply.github.com>
2022-12-01 18:35:27 -0600

    fix

--- TestOrchestrator4ML/generation/main.py
+++ TestOrchestrator4ML/generation/main.py
@@ -3,9 +3,9 @@ import time
 import datetime 
 import os 
 import pandas as pd
-import py_parser 
+from . import py_parser 
 import numpy as np 
-import label_perturbation_main
+from . import label_perturbation_main
 
 
 def giveTimeStamp():
