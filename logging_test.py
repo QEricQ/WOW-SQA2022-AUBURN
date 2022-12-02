f9dff4a9e67c45de3aa62d9e1e16d73202944e92
QEricQ <114453425+QEricQ@users.noreply.github.com>
2022-12-01 21:46:38 -0600

    Update logging_test.py

--- logging_test.py
+++ logging_test.py
@@ -1,7 +1,6 @@
 import logging
 
 def getSQALogger():
-    logging.basicConfig(filename='Forensics.Test.log', level=logging.INFO, format='%(asctime)s:%(name)s:%(levelname)s:%(message)s', datefmt='%d-%b-%y %H-%M-%S')
+    logging.basicConfig(filename='Forensics.Test.log', level=logging.DEBUG, format='%(asctime)s:%(name)s:%(levelname)s:%(message)s', datefmt='%d-%b-%y %H-%M-%S')
     logObj = logging.getLogger('sqa-logger')
-    logObj.setLevel(logging.DEBUG)
     return logObj 
